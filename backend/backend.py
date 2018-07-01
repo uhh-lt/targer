e6118b6d60bd8d7ecb7118b2f0e656d25604dcda
username3020 <38323546+username3020@users.noreply.github.com>
2018-07-01 13:19:56 +0100

    Updated backend.py for POST body requests

--- backend/backend.py
+++ backend/backend.py
@@ -3,7 +3,7 @@
 """be.py: Description."""
 from flask import Flask, jsonify, request
 from flasgger import Swagger, LazyString, LazyJSONEncoder
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from flask import make_response
 from nltk.tokenize import sent_tokenize, word_tokenize
 import random
@@ -209,11 +209,141 @@ class InputtextWDk(Resource):
        response.headers['content-type'] = 'application/json'
        return response
 
+
+class ClassifyES(Resource):
+    def post(self):
+       """
+       Takes a input sequence and assigns label with highes probability (ES model)
+       ---
+       consumes:
+         - text/plain
+       parameters:
+         - in: body
+           name: text
+           type: string
+           required: true
+           description: Text to classify 
+           example: This is an example sentence. You can paste in any other text, because this is a text field. The quotes are not needed.
+       responses:
+         200:
+           description: A list of tagged tokens annotated with labels
+           schema:
+             id: Returntext
+             properties:
+               returntext:
+                 type: string
+                 description: JSON-List
+                 default: No input text set
+        """
+       inputtext = request.get_data().decode('UTF-8')
+       result = modelES.label(inputtext)
+       response = make_response(result)
+       response.headers['content-type'] = 'application/json'
+       return response
+       
+class ClassifyWD(Resource):
+    def post(self):
+       """
+       Takes a input sequence and assigns label with highes probability (WD model)
+       ---
+       consumes:
+         - text/plain
+       parameters:
+         - in: body
+           name: text
+           type: string
+           required: true
+           description: Text to classify 
+           example: This is an example sentence. You can paste in any other text, because this is a text field. The quotes are not needed.
+       responses:
+         200:
+           description: A list of tagged tokens annotated with labels
+           schema:
+             id: Returntext
+             properties:
+               returntext:
+                 type: string
+                 description: JSON-List
+                 default: No input text set
+        """
+       inputtext = request.get_data().decode('UTF-8')
+       result = modelWD.label(inputtext)
+       response = make_response(result)
+       response.headers['content-type'] = 'application/json'
+       return response
+       
+class ClassifyESk(Resource):
+    def post(self):
+       """
+       Takes a input sequence and assigns label with highes probability (ESk model)
+       ---
+       consumes:
+         - text/plain
+       parameters:
+         - in: body
+           name: text
+           type: string
+           required: true
+           description: Text to classify 
+           example: This is an example sentence. You can paste in any other text, because this is a text field. The quotes are not needed.
+       responses:
+         200:
+           description: A list of tagged tokens annotated with labels
+           schema:
+             id: Returntext
+             properties:
+               returntext:
+                 type: string
+                 description: JSON-List
+                 default: No input text set
+        """
+       inputtext = request.get_data().decode('UTF-8')
+       result = modelESk.label(inputtext)
+       response = make_response(result)
+       response.headers['content-type'] = 'application/json'
+       return response
+       
+class ClassifyWDk(Resource):
+    def post(self):
+       """
+       Takes a input sequence and assigns label with highes probability (WDk model)
+       ---
+       consumes:
+         - text/plain
+       parameters:
+         - in: body
+           name: text
+           type: string
+           required: true
+           description: Text to classify 
+           example: This is an example sentence. You can paste in any other text, because this is a text field. The quotes are not needed.
+       responses:
+         200:
+           description: A list of tagged tokens annotated with labels
+           schema:
+             id: Returntext
+             properties:
+               returntext:
+                 type: string
+                 description: JSON-List
+                 default: No input text set
+        """
+       inputtext = request.get_data().decode('UTF-8')
+       result = modelWDk.label(inputtext)
+       response = make_response(result)
+       response.headers['content-type'] = 'application/json'
+       return response
+       
 api.add_resource(Inputtext, '/inputtext/<inputtext>')
 api.add_resource(InputtextES, '/inputtextES/<inputtext>')
 api.add_resource(InputtextWD, '/inputtextWD/<inputtext>')
 api.add_resource(InputtextESk, '/inputtextESk/<inputtext>')
 api.add_resource(InputtextWDk, '/inputtextWDk/<inputtext>')
+api.add_resource(ClassifyES, '/classifyES')
+api.add_resource(ClassifyWD, '/classifyWD')
+api.add_resource(ClassifyESk, '/classifyESk')
+api.add_resource(ClassifyWDk, '/classifyWDk')
+
 
 app.run(host='0.0.0.0', port=6000,debug=False)
 
