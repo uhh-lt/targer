90cd45795af8b2967d95565849677308f5a18a9e
alexteua <oleksiy.oliynyk@gmail.com>
2018-10-09 03:40:33 +0200

    better class identification

--- batch_processing/es_indexing/Word.py
+++ batch_processing/es_indexing/Word.py
@@ -3,5 +3,5 @@ class Word:
     def __init__(self, text):
         fields = text.split()
         self.FORM = fields[1]
-        self.ENTITY = fields[8]
-        self.ARGUMENT = fields[9]
+        self.ENTITY = fields[8].replace("B-", "").replace("I-", "")
+        self.ARGUMENT = fields[9].replace("-B", "").replace("-I", "")
