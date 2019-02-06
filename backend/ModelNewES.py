f6d63d85a45ae8b4571366020143e27f53cec789
alexteua <oleksiy.oliynyk@gmail.com>
2019-02-06 17:58:16 +0100

    adaptation to the new tagger

--- backend/ModelNewES.py
+++ backend/ModelNewES.py
@@ -8,6 +8,9 @@ import json
 
 path = "models/model_new_es.hdf5"
 
+def replace_labels(input):
+    return input.replace('B-Premise','P-B').replace('I-Premise','P-I').replace('I-Claim','C-I').replace('B-Claim','C-B').replace("B-MajorClaim", "C-B").replace("I-MajorClaim", "C-I")
+
 class ModelNewES:
 
     tagger =TaggerFactory.load(path, -1)
@@ -22,6 +25,8 @@ class ModelNewES:
         sentences = [ nltk.word_tokenize(sent) for sent in nltk.sent_tokenize(input)]
 
         output = self.tagger.predict_tags_from_words(sentences, batch_size=200)
+
+        output = [[replace_labels(t) for t in s] for s in output]
 
         result = []
         for sentenceIdx in range(len(sentences)):
